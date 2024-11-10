0bd5cbeb5c5dbf39c5394a60aaf18da2564a514b
Gonzalo de la Rosa <118688295+GdelaRosa05@users.noreply.github.com>
2024-11-10 21:06:08 +0100

    Terminado

--- script.js
+++ script.js
@@ -2,8 +2,34 @@ const notchItem = document.querySelectorAll('.notch-item')
 
 notchItem.forEach(item => {
     item.addEventListener('click', function(event){
-        event.preventDefault()
         notchItem.forEach(i => i.classList.remove('active'))
         this.classList.add('active')
     })
-});
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    const anchors = document.querySelectorAll('a[href^="#"]')
+
+    function onScroll() {
+        let currentAnchor = null
+
+        anchors.forEach(anchor => {
+            const section = document.querySelector(anchor.getAttribute('href'))
+
+            if (section.getBoundingClientRect().top <= 0) {
+                currentAnchor = anchor
+            }
+        })
+
+        anchors.forEach(anchor => {
+            anchor.classList.remove('active')
+        })
+
+        if (currentAnchor) {
+            currentAnchor.classList.add('active')
+        }
+    }
+
+    window.addEventListener('scroll', onScroll)
+    onScroll()
+})
