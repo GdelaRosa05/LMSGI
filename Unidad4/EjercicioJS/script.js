48c378dca35cc4124223fdbf300aad514d6f6d40
talo05 <2005gdelarosa@gmail.com>
2025-02-20 12:57:06 +0100

    Tema 4 JS Ej1

--- Unidad4/EjercicioJS/script.js
+++ Unidad4/EjercicioJS/script.js
@@ -1,44 +1,26 @@
-//APARTADO 1
-//En este caso se pide que crees un vector de 100 números y devuelvas el sumatorio de todos los números.
-let listaNum = []
+let addBtn = document.getElementById('add-btn')
+let addNumInput = document.getElementById('add-num-input')
+let countWords = document.getElementById('count-words')
+let errorActive = false
+let numeros = []
 
-for(let j = 0; j <= 100; j++){
-    listaNum.push(j)
-}
-
-let sumaNum = 0
-
-for(let i = 0; i < listaNum.length; i++){
-    sumaNum += listaNum[i]
-}
-
-document.write("La suma de todos los números del array es: " + sumaNum)
-
-// APARTADO 2
-//Diseña un script que vaya leyendo números y guardándolos en un array. Una vez lleno mostrará el array y deberá decir cuantos números son pares y cuantos son impares. La entrada de datos termina cuando el usuario teclea 0 o un valor no numérico.
-
-//Por ejemplo si tecleo: 3,5,4,6,7,8, 11 el script me dirá que el array de entrada es [3,5,4,6,7,8, 11] y que hay 3 pares y 4 impares.
-const numeros = [];
-let pares = 0;
-let impares = 0;
-
-while (true) {
-    let input = prompt("Introduce un número (0 para salir):");
-
-    if (input === null || isNaN(input) || input.trim() === "" || Number(input) === 0) {
-        break;
+addBtn.addEventListener('click', function(){
+    if (addNumInput.value === ""){
+        addNumInput.placeholder = "Introduce un número"
+        addNumInput.classList.add("errorPlaceholder")
     }
 
-    let numero = Number(input);
-    numeros.push(numero);
-
-    if (numero % 2 === 0) {
-        pares++;
-    } else {
-        impares++;
-    }
-}
-
-console.log(`Array de entrada: [${numeros.join(", ")}]`);
-console.log(`Números pares: ${pares}`);
-console.log(`Números impares: ${impares}`);
+    if (addNumInput.value !== ""){
+        errorActive = false
+        addNumInput.placeholder = "Example: 34"
+        addNumInput.classList.remove("errorPlaceholder")
+        addNumInput.classList.add("formatPlaceholder")
+        countWords.textContent = parseInt(countWords.textContent) + 1
+        numeros.push(countWords.textContent)
+        addNumInput.value = ""
+        for (let i = 0; i < numeros.length; i++) {
+            console.log(`Índice ${i}: ${numeros[i]}`);
+        }
+        
+    }  
+})
